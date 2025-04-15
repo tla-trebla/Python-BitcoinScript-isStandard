22ed75380d5725eac6c6d1a32b8c8b62eae2ed7b
Albert Pangestu <stamina.left@gmail.com>
2025-04-15 16:13:12 +0700

    Added test case if it's an OP_RETURN script

--- bitcoin_script_test/test_script.py
+++ bitcoin_script_test/test_script.py
@@ -1,6 +1,6 @@
 from bitcoin.wallet import CBitcoinAddress
-from bitcoin.core import CScript
-from bitcoin.core.script import OP_CHECKSIG, OP_DUP, OP_HASH160, OP_EQUALVERIFY, OP_EQUAL
+from bitcoin.core import CScript, x
+from bitcoin.core.script import OP_CHECKSIG, OP_DUP, OP_HASH160, OP_EQUALVERIFY, OP_EQUAL, OP_RETURN
 
 def is_p2pkh(script):
     ops = list(script)
@@ -22,6 +22,18 @@ def is_p2sh(script):
         ops[2] == OP_EQUAL
     )
 
+def is_op_return(script):
+    try:
+        ops = list(script)
+        return (
+            len(ops) >= 1 and
+            ops[0] == OP_RETURN and
+            all(isinstance(op, (bytes, int)) for op in ops[1:])
+        )
+    except Exception as e:
+        print(f"Error parsing script: {e}")
+        return False
+
 def test_script(address_str):
     addr = CBitcoinAddress(address_str)
     script = addr.to_scriptPubKey()
@@ -36,5 +48,21 @@ def test_script(address_str):
     else:
         print("This is not a P2PKH script.")
 
+    print("---------------------------")
+
+def test_op_return_script(address_str):
+    script = CScript([OP_RETURN, address_str.encode()])
+
+    print("Script:", script)
+    print("Hex:", script.hex())
+
+    if is_op_return(script):
+        print("This is an OP_RETURN script.")
+    else:
+        print("This is not an OP_RETURN script.")
+    
+    print("---------------------------")
+
 test_script("1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa")
 test_script("3AdD7ZaJQw9m1maN39CeJ1zVyXQLn2MEHR")
+test_op_return_script("1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa")
