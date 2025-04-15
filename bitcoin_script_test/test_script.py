3a62a56fd1af97a69323561c856e9b410a95dffb
Albert Pangestu <stamina.left@gmail.com>
2025-04-15 15:27:32 +0700

    Added another check if it's a P2SH script

--- bitcoin_script_test/test_script.py
+++ bitcoin_script_test/test_script.py
@@ -1,6 +1,6 @@
 from bitcoin.wallet import CBitcoinAddress
 from bitcoin.core import CScript
-from bitcoin.core.script import OP_CHECKSIG, OP_DUP, OP_HASH160, OP_EQUALVERIFY
+from bitcoin.core.script import OP_CHECKSIG, OP_DUP, OP_HASH160, OP_EQUALVERIFY, OP_EQUAL
 
 def is_p2pkh(script):
     ops = list(script)
@@ -13,6 +13,15 @@ def is_p2pkh(script):
         ops[4] == OP_CHECKSIG
     )
 
+def is_p2sh(script):
+    ops = list(script)
+    return (
+        len(ops) == 3 and
+        ops[0] == OP_HASH160 and
+        isinstance(ops[1], bytes) and len(ops[1]) == 20 and
+        ops[2] == OP_EQUAL
+    )
+
 def test_script(address_str):
     addr = CBitcoinAddress(address_str)
     script = addr.to_scriptPubKey()
@@ -22,7 +31,10 @@ def test_script(address_str):
 
     if is_p2pkh(script):
         print("This is a P2PKH script.")
+    elif is_p2sh(script):
+        print("This is a P2SH script.")
     else:
         print("This is not a P2PKH script.")
 
 test_script("1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa")
+test_script("3AdD7ZaJQw9m1maN39CeJ1zVyXQLn2MEHR")
